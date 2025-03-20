160a4177a3856ae73f800930944e56bd03c60bf0
ghosts-lk <ghosts.lk@proton.me>
2025-03-20 17:56:18 +0530

    .

---
+++ test/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="px-5 py-10 text-xs text-center border-t-2 border-solid border-t-green-500">
+      <p>© 2024 Ghost Protocol. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
